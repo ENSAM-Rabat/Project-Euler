51310ce6230f0be3190efa6f1012fdf527a0a5f0
Mohamed ELBAKKALI <36137367+MohamedELBAKKALI@users.noreply.github.com>
2022-01-31 21:29:52 +0100

    Update s.py

--- Problem 001/solutions/MohamedELBAKKALI/s.py
+++ Problem 001/solutions/MohamedELBAKKALI/s.py
@@ -1,2 +1,24 @@
 # Test mokamed
-# cooment 1
+content = []
+file1 = open('input.txt', 'r')
+Lines = file1.readlines()
+iter = 0;
+for line in Lines:
+    content.append(line.replace('\n', '').split(" "))
+    if iter == 0 :
+        print("this file contans ",int(line),"operatons")
+    else:
+        print("Operation ",iter," :")
+        D, L, R  = list(map(int, line.split()))
+        if D <= R and D>= L:
+            print('Take second dose now')
+        elif D > R:
+            print('Too Late')
+        elif D < L:
+            print('Too Early')
+    iter = iter+1
+print("----- content -----")
+print(content)
+
+
+    
