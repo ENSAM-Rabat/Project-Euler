7ec88b30ae9b7448937dc2a1422cef7a9c440345
Lariouch Oussama <90968210+LariouchOussama@users.noreply.github.com>
2022-02-04 16:41:44 +0100

    Add files via upload

---
+++ Problem 004/solutions/LariouchOussama/Largest_palindrome_product.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void main(){
+    int x, y;
+    int prod, revprod, reverse, max;
+
+    max=0;
+    for(x=100; x<1000; x++){
+
+        for ( y = 100; y < 1000; y++)
+        {
+            prod=x*y;
+            revprod=prod;
+            reverse=0;
+            while (revprod > 0)
+            {
+                reverse=reverse * 10 +revprod %10 ;
+                revprod /= 10;
+
+            }
+            if (reverse == prod){
+                if (prod > max)
+                {
+                    max=prod;
+                }
+                
+            }
+        }
+        
+    }
+    printf("%d", max);
+
+}
