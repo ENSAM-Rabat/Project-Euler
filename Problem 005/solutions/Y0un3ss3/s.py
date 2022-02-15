93a87bfbcfb9f483de13d012add3e1b938d7a718
Younes <y0un3ss3@users.noreply.github.com>
2022-02-15 16:14:33 +0100

    Youne Youness Solution" "

---
+++ Problem 005/solutions/Y0un3ss3/s.py
@@ -0,0 +1,16 @@
+"""
+The first number evenly divisible by range [1, 20]
+is simply the lowest common multiple of every number in this range.
+where: LCM(a, b) = a * b // GCD(a, b)
+"""
+from math import gcd
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def rlcm(end, s=1):
+    if end != 0:
+        return rlcm(end-1, lcm(s, end))
+    return s
+
+print(rlcm(20)) # output: 232792560
